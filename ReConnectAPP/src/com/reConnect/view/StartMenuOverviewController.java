9a91b293a0dfb226fd1b5591023c4aa5e1fb2084
Pchaoz <polsh2001.1@gmail.com>
2021-05-24 10:59:19 +0200

    Start menu created and working

---
+++ ReConnectAPP/src/com/reConnect/view/StartMenuOverviewController.java
@@ -0,0 +1,56 @@
+package com.reConnect.view;
+
+import java.io.IOException;
+
+import com.reConnect.MainApp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class StartMenuOverviewController {
+
+	@FXML
+	private Label welcomeText;
+	
+	@FXML
+	private Button signInButton;
+	
+	@FXML
+	private Button signUpButton;
+	
+	@FXML
+	private void initialize() {		
+		
+	}
+	
+	/*
+	 * Botó per a iniciar sessió
+	 */
+	@FXML
+    private void handleChangeLogin(ActionEvent event) throws IOException{
+            Parent root = FXMLLoader.load(getClass().getResource("SignInOverview.fxml"));
+            Scene newScene = new Scene(root);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(newScene);
+            window.show();
+    }
+	
+	@FXML
+    private void handleChangeSignUp(ActionEvent event) throws IOException{
+            Parent root = FXMLLoader.load(getClass().getResource("SignUpOverview.fxml"));
+            Scene newScene = new Scene(root);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(newScene);
+            window.show();
+    }
+
+
+}
+
