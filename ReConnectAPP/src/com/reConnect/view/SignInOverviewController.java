0a0e056a5995bc477b4f99a705e33ce08519dc24
Pchaoz <polsh2001.1@gmail.com>
2021-05-25 11:24:44 +0200

    Update 25/05/21 11:22 - Login works correctly

--- ReConnectAPP/src/com/reConnect/view/SignInOverviewController.java
+++ ReConnectAPP/src/com/reConnect/view/SignInOverviewController.java
@@ -1,14 +1,22 @@
 package com.reConnect.view;
 
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import com.reConnect.model.UserDAO;
 import com.reConnect.model.UserVO;
 import com.reConnect.util.HashPassword;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class SignInOverviewController {
 
@@ -27,12 +35,12 @@ public class SignInOverviewController {
 	public void initialize() {
 
 	}
-	
+
 	@FXML
-	private void handleLogin() throws NoSuchAlgorithmException {
+	public void handleLogin(ActionEvent event) throws IOException, NoSuchAlgorithmException {
 		
-		/* VARIABLES */
-		
+	/* VARIABLES */
+
 		HashPassword hash = new HashPassword();
 		
 		String username = usernameField.getText();
@@ -42,16 +50,20 @@ public class SignInOverviewController {
 		UserVO loginUser = new UserVO(username, hashedPassword);
 		UserDAO validateUser = new UserDAO();
 		
-		/* VALIDATES THE USER */
 		
-		validateUser.validateUser(loginUser);
+		/* VALIDATES THE USER  */
 		
 		
+		if (validateUser.validateUser(loginUser)) {
+		
+			Parent root = FXMLLoader.load(getClass().getResource("StartMenuOverview.fxml"));
+	        Scene newScene = new Scene(root);
+	        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+	        window.setScene(newScene);
+	        window.show();
+		}
 		
 	}
-	
-	
-	
 }
 
 
